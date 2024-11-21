8fa30cbb3d45bc7345780672e66ee06b0e2e17c3
Programmer <rialcoin.io@gmail.com>
2024-11-21 07:05:25 +0400

    Add tonconnect-manifest

---
+++ src/components/Buy.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { InvitationModal } from "./InvitationModal";
+import dashboard from "../assets/images/dashboard.jpg";
+import { TonConnectUIProvider, TonConnectButton } from "@tonconnect/ui-react";
+import Send from "./Send";
+
+export const Buy = () => {
+  const manifestUrl = "https://rialcoin.io/tonconnect-manifest.json";
+
+  return (
+
+      <TonConnectUIProvider manifestUrl={manifestUrl}>
+        <Send client:only="react" />
+      </TonConnectUIProvider>
+  );
+};
