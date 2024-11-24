e2c0d74793e597ae3c85f916ca036e45bb650d1c
Programmer <rialcoin.io@gmail.com>
2024-11-24 13:00:46 +0400

    Removed top and bottom shape dividers, and background color classes from FeaturesDiagonal component.

--- src/components/FeaturesDiagonal.jsx
+++ src/components/FeaturesDiagonal.jsx
@@ -3,22 +3,8 @@ import featuresdiagonal from "../assets/images/featuresdiagonal.jpg";
 
 export const FeaturesDiagonal = () => {
   return (
-    <section className="lg:mb-16 w-full flex flex-col justify-center items-center bg-bgDark1">
-      {/* Top Shape Divider */}
-      <div className="shape-divider-bottom-1665696614">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-          className="bg-bgDark2 fill-bgDark2"
-        >
-          <path
-            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
-            className="bg-bgDark1 fill-bgDark1"
-          ></path>
-        </svg>
-      </div>
+    <section className="lg:mb-16 w-full flex-col justify-center items-center">
+ 
 
       {/* Content Section */}
       <motion.div
@@ -27,7 +13,7 @@ export const FeaturesDiagonal = () => {
         viewport={{ once: true }}
         transition={{ duration: 0.5, delay: 0.2 }}
       >
-        <div className="2xl:w-[1150px] xl:w-[1050px] md:w-4/5 flex justify-center bg-bgDark1 pt-12 lg:pt-24 pb-8 lg:pb-20 mx-auto lg:flex-row flex-col">
+        <div className="2xl:w-[1150px] xl:w-[1050px] md:w-4/5 flex justify-center pt-12 lg:pt-24 pb-8 lg:pb-20 mx-auto lg:flex-row flex-col">
           {/* Left Content */}
           <div className="w-3/4 lg:w-1/2 flex flex-col lg:mx-unset mx-auto">
             <span className="block-subtitle text-primaryColor">
@@ -70,21 +56,7 @@ export const FeaturesDiagonal = () => {
         </div>
       </motion.div>
 
-      {/* Bottom Shape Divider */}
-      <div className="shape-divider-top-1665696661 w-full">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-          className="bg-bgDark2 fill-bgDark2"
-        >
-          <path
-            d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
-            className="bg-bgDark1 fill-bgDark1"
-          ></path>
-        </svg>
-      </div>
+    
     </section>
   );
 };
